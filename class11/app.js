c09beda63f926d2bafbe82f4dc48012f83b34be2
rasool bashir <bashirrasool0@gmail.com>
2023-10-04 09:13:06 -0700

    new class

---
+++ class11/app.js
@@ -0,0 +1,18 @@
+     // MATHS OBJECT
+    //  Math.round(Math.random() * 5) + 1
+
+
+     // date
+
+    //  const d = new Date();
+    //  document.getElementById("demo").innerHTML = d
+
+
+
+
+     
+const d = new Date(2018, 11, 24, 10, 33, 30, 0);
+document.getElementById("demo").innerHTML = d;
+
+
+

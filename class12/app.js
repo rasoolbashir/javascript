334eb7e16d0073286dc058f47717acd830833c34
rasool bashir <bashirrasool0@gmail.com>
2023-10-06 09:43:18 -0700

    adding new class work

---
+++ class12/app.js
@@ -0,0 +1,47 @@
+// var text =("i am osama")
+
+// text.replace("Rasool")
+ 
+
+       //geating the current date and time
+               // (1)
+    //    var rightNow = new Date();
+    //    var theDay = rightNow.getDay();
+
+       
+// function
+//  function greetUser(greeting) {
+//      alert(greeting);
+//      }
+
+       // global scope and local scope
+//        var thesum
+// addNumbers();
+//  function addNumbers() {
+//      var theSum = 7 + 18;
+//      alert(theSum)
+//      }
+
+// var thesum =1000
+// addNumbers();
+//  function addNumbers() {
+//      var theSum = 4 + 4;
+   
+//      }
+//      console.log("theSum")
+        
+
+             // switch statement
+              switch(dayOfWk) {
+                 case "Sat" :
+                 alert("Whoopee");
+                 break;
+                 case "Sun" :
+             alert("Whoopee");
+                 break;
+                 case "Fri" :
+                 alert("TGIF!");
+                 break;
+                 default :
+                 alert("Shoot me now!");
+                 }
